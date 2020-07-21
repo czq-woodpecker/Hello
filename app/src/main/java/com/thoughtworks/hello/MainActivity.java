8d0e62c9c2e743672b6a9d2e58dd5e0e9bc98908
zeqiang.chen <1209120868@qq.com>
2020-07-22 01:21:15 +0800

    [zeqiang] feat: add constrain activity

--- app/src/main/java/com/thoughtworks/hello/MainActivity.java
+++ app/src/main/java/com/thoughtworks/hello/MainActivity.java
@@ -2,8 +2,11 @@ package com.thoughtworks.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
@@ -14,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "MainActivity Created");
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openConstraintActivity();
+            }
+        });
+    }
+
+    public void openConstraintActivity() {
+        Intent intent = new Intent(this, ConstraintActivity.class);
+        startActivity(intent);
     }
 }
